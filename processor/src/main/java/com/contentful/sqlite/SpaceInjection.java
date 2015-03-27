46dccf35b30aecebd84dfb8863481935c655877a
Tom Reznik <tom@contentful.com>
2015-03-27 23:20:29 +0100

    Execute default create statements

--- processor/src/main/java/com/contentful/sqlite/SpaceInjection.java
+++ processor/src/main/java/com/contentful/sqlite/SpaceInjection.java
@@ -21,6 +21,7 @@ final class SpaceInjection extends Injection {
     // Emit: imports
     builder.append("import android.database.sqlite.SQLiteOpenHelper;\n")
         .append("import android.database.sqlite.SQLiteDatabase;\n")
+        .append("import com.contentful.sqlite.DbHelper;\n")
         .append("import android.content.Context;\n\n");
 
     // Emit: class
@@ -28,20 +29,29 @@ final class SpaceInjection extends Injection {
         .append(className)
         .append(" extends SQLiteOpenHelper {\n");
 
+    // Emit: fields
+    builder.append("  final DbHelper dbHelper;\n\n");
+
     // Emit: constructor
     builder.append("  public ")
         .append(className)
-        .append("(Context context) {\n")
+        .append("(Context context, DbHelper dbHelper) {\n")
         .append("    super(context, null, null, 1);\n")
+        .append("    this.dbHelper = dbHelper;\n")
         .append("  }\n\n");
 
     // Emit: onCreate
     builder.append("  @Override public void onCreate(SQLiteDatabase db) {\n");
 
-    // Emit: create model tables statements
     builder.append("    db.beginTransaction();\n")
         .append("    try {\n");
 
+    // Emit: CREATE default statements
+    builder.append("      for (String sql : dbHelper.getDefaultCreateStatements()) {\n")
+        .append("        db.execSQL(sql);\n")
+        .append("      }\n\n");
+
+    // Emit: CREATE model tables
     for (ModelInjection model : models) {
       List<String> createStatements = model.getCreateStatements("      ");
       for (int i = 0; i < createStatements.size(); i++) {
--- sqlite/src/main/java/com/contentful/sqlite/DbHelper.java
+++ sqlite/src/main/java/com/contentful/sqlite/DbHelper.java
@@ -1,4 +1,30 @@
 package com.contentful.sqlite;
 
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class DbHelper {
+  public static final String TABLE_ASSETS = "assets";
+  public static final String TABLE_LINKS = "links";
+
+  static final String CREATE_ASSETS = "CREATE TABLE `"
+      + TABLE_ASSETS
+      + "` ("
+      + "`_ID` INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + "`remote_id` STRING NOT NULL,"
+      + "`url` STRING NOT NULL,"
+      + "`mime_type` STRING NOT NULL"
+      + ");";
+
+  static final String CREATE_LINKS = "CREATE TABLE `"
+      + TABLE_LINKS
+      + "` ("
+      + "`_ID` INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + "`parent` STRING NOT NULL,"
+      + "`child` STRING NOT NULL"
+      + ");";
+
+  public static List<String> getDefaultCreateStatements() {
+    return Arrays.asList(CREATE_ASSETS, CREATE_LINKS);
+  }
 }
