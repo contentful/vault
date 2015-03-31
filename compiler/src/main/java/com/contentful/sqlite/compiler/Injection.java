2d69a5ceb138010fb37d6ff82870a904c14c5328
Tom Reznik <tom@contentful.com>
2015-03-31 11:14:34 +0200

    Rename Maven artifacts

---
+++ compiler/src/main/java/com/contentful/sqlite/compiler/Injection.java
@@ -0,0 +1,21 @@
+package com.contentful.sqlite.compiler;
+
+abstract class Injection {
+  final String id;
+  final String classPackage;
+  final String className;
+  final String targetClass;
+
+  public Injection(String id, String classPackage, String className, String targetClass) {
+    this.id = id;
+    this.classPackage = classPackage;
+    this.className = className;
+    this.targetClass = targetClass;
+  }
+
+  abstract String brewJava();
+
+  String getFqcn() {
+    return classPackage + "." + className;
+  }
+}
