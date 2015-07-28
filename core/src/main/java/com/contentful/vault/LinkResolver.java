b06312bb0a0010c464058e26f7859e360d03004b
Tom Reznik <xoreznik@gmail.com>
2015-07-28 11:02:31 +0200

    Add missing copyright headers

--- core/src/main/java/com/contentful/vault/LinkResolver.java
+++ core/src/main/java/com/contentful/vault/LinkResolver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Contentful GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.contentful.vault;
 
 import android.database.Cursor;
@@ -25,7 +41,9 @@ final class LinkResolver {
       "SELECT `type_id` FROM %s WHERE %s = ?", SpaceHelper.TABLE_ENTRY_TYPES, REMOTE_ID);
 
   private final Query<?> query;
+
   private final Map<String, Resource> assets;
+
   private final Map<String, Resource> entries;
 
   LinkResolver(Query<?> query, Map<String, Resource> assets, Map<String, Resource> entries) {
